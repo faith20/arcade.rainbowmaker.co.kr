bfb67532f4d8eab91420ce475bfb5a93cbcc9497
faith20 <faithfin@naver.com>
2020-08-21 02:17:21 +0900

    Add index.html meta

--- src/App.svelte
+++ src/App.svelte
@@ -26,9 +26,5 @@
 
 <main>
   <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+  <p>Welcome.</p>
 </main>
